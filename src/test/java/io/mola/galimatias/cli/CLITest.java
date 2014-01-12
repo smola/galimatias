f50820aa59269535f4c1ddbe9c4c5e657d82a417
Santiago M. Mola <santi@mola.io>
2014-01-12 12:42:12 +0100

    Add basic test for CLI.

---
+++ src/test/java/io/mola/galimatias/cli/CLITest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2014 Santiago M. Mola <santi@mola.io>
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ *   copy of this software and associated documentation files (the "Software"),
+ *   to deal in the Software without restriction, including without limitation
+ *   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *   and/or sell copies of the Software, and to permit persons to whom the
+ *   Software is furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *   DEALINGS IN THE SOFTWARE.
+ */
+
+package io.mola.galimatias.cli;
+
+import io.mola.galimatias.GalimatiasParseException;
+import io.mola.galimatias.TestURL3;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Theories.class)
+public class CLITest {
+
+    @Theory
+    public void parse_no_exceptions(final @TestURL3.TestURLs(dataset = TestURL3.DATASETS.WHATWG)
+                                     TestURL3 testURL) throws GalimatiasParseException {
+        CLI.main(new String[]{ testURL.rawURL });
+    }
+
+}
